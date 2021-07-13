0f7f066e0eab7e975887c9fea010659b1ce04744
kk <1009576562@qq.com>
2021-07-13 17:09:53 +0800

    1

--- module/playlist_myhot.js
+++ module/playlist_myhot.js
@@ -1,8 +1,23 @@
-// one api 最近一月的
+//  
 
 module.exports = (query, request) => {
+  const data = {
+    id: query.id,
+    n: 100000,
+    s: query.s || 8
+  }
   return request(
-    'GET', `https://music.163.com/api/playlist/detail?id=${query.id}`, {},
-    {crypto: 'myapi', cookie: query.cookie, proxy: query.proxy}
+    'POST', `https://music.163.com/weapi/v3/playlist/detail`, data,
+    {crypto: 'linuxapi', cookie: query.cookie, proxy: query.proxy}
   )
 }
+
+// module.exports = (query, request) => {
+//   return request(
+//     'GET', `https://music.163.com/api/playlist/detail?id=${query.id}`, {},
+//     {crypto: 'myapi', cookie: query.cookie, proxy: query.proxy}
+//   )
+
+
+
+// }
